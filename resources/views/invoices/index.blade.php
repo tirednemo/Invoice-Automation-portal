731af73bd53807615f57de6787776865db71bedb
tirednemo <ummetasnim@iut-dhaka.edu>
2023-07-05 18:14:55 +0600

    added invoice add and details buttons

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -1,12 +1,6 @@
 <x-app-layout>
-    <div class="max-w-5xl mx-auto p-4 sm:p-6 lg:p-8">
-        <!-- <form method="POST" action="{{ route('invoices.store') }}">
-            @csrf
-            <textarea name="message" placeholder="{{ __('What\'s on your mind?') }}" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">{{ old('message') }}</textarea>
-            <x-input-error :messages="$errors->get('message')" class="mt-2" />
-            <x-primary-button class="mt-4">{{ __('Store') }}</x-primary-button>
-        </form> -->
-
+    <div class="max-w-5xl mx-auto p-4 sm:p-6 lg:p-8 relative">
+        <x-primary-button class="absolute top-0 right-0 mt-4 mr-6">{{ __('Add Invoice') }}</x-primary-button>
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
             <table class="min-w-full divide-y divide-gray-200">
                 <thead>
@@ -17,6 +11,7 @@
                         <th class="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Payee</th>
                         <th class="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Total</th>
                         <th class="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Status</th>
+                        <th class="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Action</th>
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200">
@@ -41,6 +36,9 @@
                         </td>
                         <td class="px-6 py-4 whitespace-no-wrap">
                             <div class="text-sm leading-5 text-gray-900">{{ $invoice->status }}</div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap">
+                            <button class="hover:bg-gray-100 py-2 px-2 rounded focus:outline-none focus:shadow-outline">{{ __('Details') }}</button>
                         </td>
                     </tr>
                     @endforeach
