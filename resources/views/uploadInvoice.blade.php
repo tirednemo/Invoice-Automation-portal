1ad6cca875af5695beecb0a46a6723c94cd5db2c
Ifty Khan <iftikhrimrul@iut-dhaka.edu>
2023-07-06 05:16:58 +0600

    populate form partial

--- resources/views/uploadInvoice.blade.php
+++ resources/views/uploadInvoice.blade.php
@@ -11,22 +11,51 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <x-file-upload />
 
-            <button class="py-2 px-4 bg-green-600 hover:bg-green-700 text-white rounded-lg mt-4"
-                onclick="getDataByRegex()">Get Invoice
-                Data</button>
+            @if(!session('pdfData'))
+            <div id="loading" class="hidden flex items-center justify-center" style="margin-top: 100px;">
+                <svg class="animate-spin h-6 w-6 mr-3 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0012 20c4.411 0 8-3.589 8-8h-2c0 3.309-2.691 6-6 6-3.309 0-6-2.691-6-6H6c0 4.411 3.589 8 8 8z">
+                    </path>
+                </svg>
+                <span>Loading...</span>
+            </div>
+            <button id="getInvoiceBtn" class="py-2 px-4 bg-green-600 hover:bg-green-700 text-white rounded-lg mt-4"
+                onclick="getDataByRegex()">Get Invoice Data</button>
+            @endif
+
+            @if(session('pdfData'))
+            <x-pdf-data-form-test />
+            @endif
         </div>
     </div>
 
 
     <script>
     function getDataByRegex() {
-        axios.get('/api/data')
+        var loadingElement = document.getElementById('loading');
+        var getInvoiceBtn = document.getElementById('getInvoiceBtn');
+        loadingElement.classList.remove('hidden');
+        getInvoiceBtn.style.display = 'none';
+
+        axios.get('http://localhost:3000/api/regex')
             .then(function(response) {
-                // Handle successful response
                 console.log(response.data);
+
+                axios.post('/store-data-in-session', {
+                        data: response.data
+                    })
+                    .then(function(response) {
+                        console.log(response);
+                        location.reload();
+                    })
+                    .catch(function(error) {
+                        console.log(error);
+                    });
             })
             .catch(function(error) {
-                // Handle error
                 console.log(error);
             });
     }
@@ -63,5 +92,21 @@
     }).showToast();
     </script>
     @endif
+
+    <style>
+    .animate-spin {
+        animation: spin 1s linear infinite;
+    }
+
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+    </style>
 
 </x-app-layout>
