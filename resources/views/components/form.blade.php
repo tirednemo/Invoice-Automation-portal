413f003ffb8e986243d17edbadafe6c32b3fd3e3
tirednemo <ummetasnim@iut-dhaka.edu>
2023-07-06 12:08:53 +0600

    added ugly form

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,120 @@
+<div class="max-w-3xl mx-auto mt-10 p-4 sm:p-6 lg:p-8">
+    <h1 class="text-2xl text-center font-bold mb-3">Invoice Details</h1>
+    <form method="POST" action="{{ route('invoices.store') }}" class="bg-slate-200 p-6 rounded-lg shadow-md relative">
+        @csrf
+
+        <div class="grid grid-cols-2 gap-4">
+            <div class="mb-4">
+                <label for="invoice_date" class="block text-gray-700 text-sm font-bold mb-2">Invoice Date</label>
+                <input id="invoice_date" type="datetime-local" name="invoice_date" class="form-input rounded-md shadow-sm">
+            </div>
+
+            <div class="mb-4">
+                <label for="invoice_number" class="block text-gray-700 text-sm font-bold mb-2">Invoice Number</label>
+                <input id="invoice_number" type="text" name="invoice_number" class="form-input rounded-md shadow-sm">
+            </div>
+        </div>
+
+        <div class="mb-4">
+            <label for="customer_name" class="block text-gray-700 text-sm font-bold mb-2">Customer Name</label>
+            <input id="customer_name" type="text" name="customer_name" class="form-input rounded-md shadow-sm w-64">
+        </div>
+
+        <div class="grid grid-cols-2 gap-4">
+            <div class="mb-4">
+                <label for="phone" class="block text-gray-700 text-sm font-bold mb-2">Phone</label>
+                <input id="phone" type="text" maxlength="11" name="phone" class="form-input rounded-md shadow-sm">
+            </div>
+
+            <div class="mb-4">
+                <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
+                <input id="email" type="email" name="email" class="form-input rounded-md shadow-sm w-64">
+            </div>
+        </div>
+
+        <div class="grid grid-cols-2 gap-4">
+            <div class="mb-4">
+                <label for="billing_address" class="block text-gray-700 text-sm font-bold mb-2">Billing Address</label>
+                <textarea id="billing_address" type="text" name="billing_address" class="form-input rounded-md shadow-sm w-64"></textarea>
+            </div>
+
+            <div class="mb-4">
+                <label for="shipping_address" class="block text-gray-700 text-sm font-bold mb-2">Shipping Address</label>
+                <textarea id="shipping_address" type="text" name="shipping_address" class="form-input rounded-md shadow-sm w-64"></textarea>
+            </div>
+        </div>
+
+        <div class="mb-8 mt-4">
+            <h2 class="text-lg font-bold mb-2">Item Details</h2>
+            <div id="item-details-container">
+                <div class="grid grid-cols-5 gap-3 item-details-row">
+                    <div>
+                        <label class="block text-gray-700 text-sm font-bold mb-2">Item Name</label>
+                        <input type="text" name="item_name[]" class="form-input rounded-md shadow-sm w-32">
+                    </div>
+                    <div>
+                        <label class="block text-gray-700 text-sm font-bold mb-2">Unit Price</label>
+                        <input type="number" name="unit_price[]" class="form-input rounded-md shadow-sm w-32">
+                    </div>
+                    <div>
+                        <label class="block text-gray-700 text-sm font-bold mb-2">Quantity</label>
+                        <input type="number" name="quantity[]" class="form-input rounded-md shadow-sm w-32">
+                    </div>
+                    <div>
+                        <label class="block text-gray-700 text-sm font-bold mb-2">Amount</label>
+                        <input type="number" name="amount[]" class="form-input rounded-md shadow-sm w-32">
+                    </div>
+                    <div class="mt-6">
+                        <x-primary-button id="add-item-details-btn" class="mt-2 px-7">+</x-primary-buttonn>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mb-4">
+            <label for="total_amount" class="block text-gray-700 text-sm font-bold mb-2">Total Amount</label>
+            <input id="total_amount" type="number" name="total_amount" class="form-input rounded-md shadow-sm">
+        </div>
+
+        <div class="mb-4">
+            <label for="note" class="block text-gray-700 text-sm font-bold mb-2">Note</label>
+            <textarea id="note" name="note" class="form-textarea rounded-md shadow-sm w-64"></textarea>
+        </div>
+
+        <div class="flow-root">
+            <x-primary-button class="px-6 float-right">
+                {{ __('Save') }}
+            </x-primary-button>
+        </div>
+    </form>
+</div>
+
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const addItemDetailsBtn = document.getElementById('add-item-details-btn');
+        const itemDetailsContainer = document.getElementById('item-details-container');
+
+        addItemDetailsBtn.addEventListener('click', function() {
+            const itemDetailsRow = document.createElement('div');
+            itemDetailsRow.className = 'grid grid-cols-5 gap-3 item-details-row';
+
+            const inputFields = ['item_name', 'unit_price', 'quantity', 'amount'];
+
+            inputFields.forEach(function(fieldName) {
+
+                const input = document.createElement('input');
+                input.type = 'text';
+                input.name = fieldName + '[]';
+                input.className = 'mt-1 form-input rounded-md shadow-sm w-32';
+
+                itemDetailsRow.appendChild(input);
+            });
+
+            itemDetailsContainer.appendChild(itemDetailsRow);
+
+            const rowElements = document.querySelectorAll(".item-details-row");
+            rowElements[rowElements.length - 1].appendChild(addItemDetailsBtn);
+        });
+    });
+</script>
--- resources/views/uploadInvoice.blade.php
+++ resources/views/uploadInvoice.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
+    <?php /* 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Upload Invoice') }}
         </h2>
     </x-slot>
-
-
+    */ ?>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -13,17 +13,14 @@
 
             @if(!session('pdfData'))
             <div id="loading" class="hidden flex items-center justify-center" style="margin-top: 100px;">
-                <svg class="animate-spin h-6 w-6 mr-3 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 24 24">
+                <svg class="animate-spin h-6 w-6 mr-3 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                     <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                    <path class="opacity-75" fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0012 20c4.411 0 8-3.589 8-8h-2c0 3.309-2.691 6-6 6-3.309 0-6-2.691-6-6H6c0 4.411 3.589 8 8 8z">
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0012 20c4.411 0 8-3.589 8-8h-2c0 3.309-2.691 6-6 6-3.309 0-6-2.691-6-6H6c0 4.411 3.589 8 8 8z">
                     </path>
                 </svg>
                 <span>Loading...</span>
             </div>
-            <button id="getInvoiceBtn" class="py-2 px-4 bg-green-600 hover:bg-green-700 text-white rounded-lg mt-4"
-                onclick="getDataByRegex()">Get Invoice Data</button>
+            <button id="getInvoiceBtn" class="py-2 px-4 bg-green-600 hover:bg-green-700 text-white rounded-lg mt-4" onclick="getDataByRegex()">Get Invoice Data</button>
             @endif
 
 
@@ -31,84 +28,98 @@
             @if(session('pdfData'))
             <x-pdf-data-form-test />
             @endif
+
+            <h1 class="text-2xl text-center font-bold mb-3">Choose Parsing Algorithm</h1>
+            <div class="flex justify-center">
+                <x-primary-button class="w-20  mx-5">
+                    REGEX
+                </x-primary-button>
+                <x-primary-button class="w-20 px-6 mx-5">
+                    OCR
+                </x-primary-button>
+                <x-primary-button class="w-20 px-8 mx-5">
+                    DL
+                </x-primary-button>
+            </div>
+            <x-form />
         </div>
     </div>
 
 
     <script>
-    function getDataByRegex() {
-        var loadingElement = document.getElementById('loading');
-        var getInvoiceBtn = document.getElementById('getInvoiceBtn');
-        loadingElement.classList.remove('hidden');
-        getInvoiceBtn.style.display = 'none';
+        function getDataByRegex() {
+            var loadingElement = document.getElementById('loading');
+            var getInvoiceBtn = document.getElementById('getInvoiceBtn');
+            loadingElement.classList.remove('hidden');
+            getInvoiceBtn.style.display = 'none';
 
-        axios.get('http://localhost:3000/api/regex')
-            .then(function(response) {
-                console.log(response.data);
+            axios.get('http://localhost:3000/api/regex')
+                .then(function(response) {
+                    console.log(response.data);
 
-                axios.post('/store-data-in-session', {
-                        data: response.data
-                    })
-                    .then(function(response) {
-                        console.log(response);
-                        location.reload();
-                    })
-                    .catch(function(error) {
-                        console.log(error);
-                    });
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
-    }
+                    axios.post('/store-data-in-session', {
+                            data: response.data
+                        })
+                        .then(function(response) {
+                            console.log(response);
+                            location.reload();
+                        })
+                        .catch(function(error) {
+                            console.log(error);
+                        });
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        }
     </script>
 
     @if(session('success'))
     <script>
-    Toastify({
-        text: "{{ session('success') }}",
-        duration: 3000,
-        close: true,
-        gravity: 'top',
-        position: 'right',
-        style: {
-            background: '#4BB543'
-        },
-        stopOnFocus: true
-    }).showToast();
+        Toastify({
+            text: "{{ session('success') }}",
+            duration: 3000,
+            close: true,
+            gravity: 'top',
+            position: 'right',
+            style: {
+                background: 'black'
+            },
+            stopOnFocus: true
+        }).showToast();
     </script>
     @endif
 
     @if(session('error'))
     <script>
-    Toastify({
-        text: "{{ session('error') }}",
-        duration: 3000,
-        close: true,
-        gravity: 'top',
-        position: 'right',
-        style: {
-            background: 'red'
-        },
-        stopOnFocus: true
-    }).showToast();
+        Toastify({
+            text: "{{ session('error') }}",
+            duration: 3000,
+            close: true,
+            gravity: 'top',
+            position: 'right',
+            style: {
+                background: 'red'
+            },
+            stopOnFocus: true
+        }).showToast();
     </script>
     @endif
 
     <style>
-    .animate-spin {
-        animation: spin 1s linear infinite;
-    }
-
-    @keyframes spin {
-        0% {
-            transform: rotate(0deg);
+        .animate-spin {
+            animation: spin 1s linear infinite;
         }
 
-        100% {
-            transform: rotate(360deg);
+        @keyframes spin {
+            0% {
+                transform: rotate(0deg);
+            }
+
+            100% {
+                transform: rotate(360deg);
+            }
         }
-    }
     </style>
 
 </x-app-layout>
