b9fdb0f38fb06a70896ae1c8de212047525eb929
tirednemo <ummetasnim@iut-dhaka.edu>
2023-07-06 12:08:53 +0600

    added ugly form

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -1,7 +1,13 @@
 <x-app-layout>
-    <div class="max-w-5xl mx-auto p-4 sm:p-6 lg:p-8 relative">
-        <x-primary-button class="absolute top-0 right-0 mt-4 mr-6">{{ __('Add Invoice') }}</x-primary-button>
-        <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+    <div class="max-w-5xl mx-auto p-4 sm:p-6 lg:p-8">
+
+        <div class="flow-root">
+            <x-primary-button class="mt-3 float-right">
+                <a href="{{ route('uploadInvoice') }}">{{ __('Add Invoice') }}</a>
+            </x-primary-button>
+        </div>
+
+        <div class="mt-5 bg-white shadow-sm rounded-lg divide-y">
             <table class="min-w-full divide-y divide-gray-200">
                 <thead>
                     <tr>
@@ -14,6 +20,7 @@
                         <th class="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Action</th>
                     </tr>
                 </thead>
+
                 <tbody class="bg-white divide-y divide-gray-200">
                     @foreach ($invoices as $invoice)
                     <tr>
@@ -38,7 +45,7 @@
                             <div class="text-sm leading-5 text-gray-900">{{ $invoice->status }}</div>
                         </td>
                         <td class="px-6 py-4 whitespace-no-wrap">
-                            <button class="hover:bg-gray-100 py-2 px-2 rounded focus:outline-none focus:shadow-outline">{{ __('Details') }}</button>
+                            <button class="bg-gray-100 py-2 px-2 rounded focus:outline-none focus:shadow-outline">{{ __('Details') }}</button>
                         </td>
                     </tr>
                     @endforeach
